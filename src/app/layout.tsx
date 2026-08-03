b500d3fe13339e28f6bdeaa35d49b7ac2ca9406b
FrankezeCode <Franklynezeugonna@gmail.com>
2026-08-03 02:34:26 +0100

    add impact meta tag update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ export const metadata: Metadata = {
     title: "betterdev",
     description: "Decentralized global community of engineers",
   },
+  other: {
+    "impact-site-verification": "4614aca4-91c9-4706-91b0-12dc29b5a89a",
+  },
 };
 
 export default function RootLayout({
@@ -36,12 +39,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${geistMono.variable}`}>
-      <head>
-        <meta
-          name="impact-site-verification"
-          value="4614aca4-91c9-4706-91b0-12dc29b5a89a"
-        />
-      </head>
       <body className={`${inter.className} font-sans antialiased`}>{children}</body>
     </html>
   );
